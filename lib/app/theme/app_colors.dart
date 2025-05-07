9f93e6778ddbfb481e979cb5e06c5549165560c1
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-07 11:02:12 +0700

    feat: main theme

---
+++ lib/app/theme/app_colors.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color.fromRGBO(241, 241, 237, 1);
+  static const primary = Color.fromRGBO(94, 142, 109, 1);
+  static const secondary = Color.fromRGBO(250, 247, 244, 1);
+
+  static const greyPrimary = Color.fromRGBO(87, 88, 88, 1);
+  static const greySecondary = Color.fromRGBO(25, 25, 25, 1);
+
+  static const textPrimary = Colors.black;
+}
---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import 'app_fonts.dart';
+
+class AppTheme {
+  static ThemeData appTheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.background,
+    fontFamily: AppFonts.primary,
+    colorScheme: ColorScheme.light(
+      primary: AppColors.primary,
+      secondary: AppColors.secondary,
+    ),
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(color: AppColors.textPrimary),
+      titleMedium: TextStyle(color: AppColors.textPrimary),
+      titleSmall: TextStyle(color: AppColors.textPrimary),
+      bodyLarge: TextStyle(color: AppColors.textPrimary),
+      bodyMedium: TextStyle(color: AppColors.textPrimary),
+      bodySmall: TextStyle(color: AppColors.textPrimary),
+    ),
+  );
+}
