5192e79afa134c24d7946b788aa2daa0cc92662d
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-08 13:48:11 +0700

    feat: auth services

---
+++ lib/features/auth/data/auth_service.dart
@@ -0,0 +1,61 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AuthService {
+  final _firebaseAuth = FirebaseAuth.instance;
+
+  Future<User?> signInWithEmail(String email, String password) async {
+    try {
+      final result = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+
+      return result.user;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<User?> registerWithEmail(String email, String password) async {
+    try {
+      final result = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+
+      return result.user;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<User?> signInWithGoogle() async {
+    try {
+      final googleUser = await GoogleSignIn().signIn();
+
+      if (googleUser == null) {
+        throw("Google account not found");
+      }
+
+      final googleAuth = await googleUser.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final result = await _firebaseAuth.signInWithCredential(credential);
+
+      return result.user;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<void> sendEmailVerification() async {
+    final user = _firebaseAuth.currentUser;
+    if (user != null && !user.emailVerified) {
+      await user.sendEmailVerification();
+    }
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+}
