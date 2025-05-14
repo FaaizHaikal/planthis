dc679b2ba12300624feebcd7b40f68a908e28dc0
FaaizHaikal <faaizhaikal@gmail.com>
2025-05-14 23:10:34 +0700

    feat: secondary button

--- lib/features/dashboard/presentations/dashboard_screen.dart
+++ lib/features/dashboard/presentations/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:planthis/features/dashboard/presentations/widgets/primary_button.dart';
+import 'package:planthis/features/dashboard/presentations/widgets/secondary_button.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
@@ -21,12 +22,33 @@ class DashboardScreen extends StatelessWidget {
               body: 'Rekomendasi pohon di sekitar Anda',
               onClickRoute: '/lahan-saya',
             ),
+            Row(
+              spacing: 16.0,
+              children: [
+                Expanded(
+                  child: SecondaryButton(
+                    mainIcon: FontAwesomeIcons.trowel,
+                    iconSizeRatio: 1.5,
+                    title: 'Peralatan Tanam',
+                    onClickRoute: '/peralatan-tanam',
+                  ),
+                ),
+                Expanded(
+                  child: SecondaryButton(
+                    mainIcon: FontAwesomeIcons.cloudRain,
+                    iconSizeRatio: 1.5,
+                    title: 'Ramalan Cuaca',
+                    onClickRoute: '/ramalan-cuaca',
+                  ),
+                ),
+              ],
+            ),
             PrimaryButton(
-              mainIcon: FontAwesomeIcons.calendarDays,
+              mainIcon: FontAwesomeIcons.calendarCheck,
               iconSizeRatio: 1.3,
-              title: 'Tanggal Potensial',
+              title: 'Tanggal Tanam',
               body: 'Tanggal yang cocok untuk bertanam',
-              onClickRoute: '/tanggal-potensial',
+              onClickRoute: '/tanggal-tanam',
             ),
             PrimaryButton(
               mainIcon: FontAwesomeIcons.seedling,
