b069a31f85c0eba6641db4e36e36575d5611ac03
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-17 23:52:09 +0700

    feat: lahan saya state

---
+++ lib/features/lahan_saya/states/lahan_saya_state.dart
@@ -0,0 +1,32 @@
+import 'package:planthis/features/lahan_saya/models/scan_response.dart';
+import 'package:latlong2/latlong.dart';
+
+class LahanSayaState {
+  final LatLng? selectedCoordinate;
+  final ScanResponse? scanResponse;
+  final String? error;
+
+  LahanSayaState({
+    required this.selectedCoordinate,
+    required this.scanResponse,
+    this.error
+  });
+
+  factory LahanSayaState.initial() => LahanSayaState(
+    selectedCoordinate: null, 
+    scanResponse: null,
+    error: null
+  );
+
+  LahanSayaState set({
+    LatLng? selectedCoordinate,
+    ScanResponse? scanResponse,
+    String? error,
+  }) {
+    return LahanSayaState(
+      selectedCoordinate: selectedCoordinate ?? this.selectedCoordinate, 
+      scanResponse: scanResponse ?? this.scanResponse,
+      error: error ?? this.error
+    );
+  }
+}
