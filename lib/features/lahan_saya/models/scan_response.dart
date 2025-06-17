35317db330a3148394b75c63c1cbf3051b9aeb18
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-17 23:53:08 +0700

    feat: modelize scan response

---
+++ lib/features/lahan_saya/models/scan_response.dart
@@ -0,0 +1,23 @@
+import 'package:planthis/core/models/climate.dart';
+import 'package:planthis/core/models/soil.dart';
+import 'package:planthis/core/models/tree.dart';
+
+class ScanResponse {
+  final double? altitude;
+  final Climate? climate;
+  final Soil? soil;
+  final List<String>? matchingSpecies;
+
+  const ScanResponse({required this.altitude, this.climate, this.soil, this.matchingSpecies});
+
+  factory ScanResponse.fromJson(Map<String, dynamic> json) {
+    return ScanResponse(
+      altitude: (json['altitude'] as num).toDouble(),
+      climate: json['climate'] != null ? Climate.fromJson(json['climate']) : null,
+      soil: json['soil'] != null ? Soil.fromJson(json['soil']) : null,
+      matchingSpecies: json['matching_species'] != null 
+          ? List<String>.from(json['matching_species']) 
+          : null,
+    );
+  }
+}
