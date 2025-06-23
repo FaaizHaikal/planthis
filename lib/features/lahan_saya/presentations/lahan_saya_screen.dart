f1e93eaaa9cd683cc97018ed07ae2418b4829530
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-23 17:10:08 +0700

    feat: scan info

--- lib/features/lahan_saya/presentations/lahan_saya_screen.dart
+++ lib/features/lahan_saya/presentations/lahan_saya_screen.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
+import 'package:planthis/features/lahan_saya/presentations/widgets/scan_info.dart';
 import 'package:planthis/features/lahan_saya/providers/lahan_saya_provider.dart';
 
 class LahanSayaScreen extends ConsumerWidget {
   const LahanSayaScreen({super.key});
-
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -16,44 +16,58 @@ class LahanSayaScreen extends ConsumerWidget {
     final tileProvider = FMTCTileProvider(
       stores: const {'mapStore': BrowseStoreStrategy.readUpdateCreate},
     );
-
     return Scaffold(
-      body: state.selectedCoordinate == null ? 
-      const Center(child: CircularProgressIndicator())
-      : 
-      FlutterMap(
-        options: MapOptions(
-          initialCenter: state.selectedCoordinate!,
-          minZoom: 6,
-          initialZoom: 13,
-          maxZoom: 16,
-          onTap: (tapPos, latLng) {
-            controller.setLocation(latLng);
-          },
-        ),
-        children: [
-          TileLayer(
-            tileProvider: tileProvider,
-            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-            userAgentPackageName: 'com.example.planthis',
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                point: state.selectedCoordinate!,
-                width: 40,
-                height: 40,
-                child: const Icon(Icons.location_pin, color: Colors.red, size: 40),
-              )
-            ],
-          ),
-        ],
-      ),
+      body:
+          state.selectedCoordinate == null
+              ? const Center(child: CircularProgressIndicator())
+              : Stack(
+                children: [
+                  FlutterMap(
+                    options: MapOptions(
+                      initialCenter: state.selectedCoordinate!,
+                      minZoom: 6,
+                      initialZoom: 13,
+                      maxZoom: 16,
+                      onTap: (tapPos, latLng) {
+                        controller.setLocation(latLng);
+                      },
+                    ),
+                    children: [
+                      TileLayer(
+                        tileProvider: tileProvider,
+                        urlTemplate:
+                            "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                        userAgentPackageName: 'com.example.planthis',
+                      ),
+                      MarkerLayer(
+                        markers: [
+                          Marker(
+                            point: state.selectedCoordinate!,
+                            width: 40,
+                            height: 40,
+                            child: const Icon(
+                              Icons.location_pin,
+                              color: Colors.red,
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  if (state.scanResponse != null)
+                    Positioned(
+                      left: 16,
+                      bottom: 90,
+                      child: ScanInfo(scan: state.scanResponse!),
+                    ),
+                ],
+              ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          print("Scanned: ${state.selectedCoordinate!.latitude}, ${state.selectedCoordinate!.longitude}");
+        onPressed: () async {
+          await controller.scanLocation();
         },
-        label: const Text("Scan this point"),
+        label: const Text("Scan"),
         icon: const Icon(Icons.radar),
       ),
     );
