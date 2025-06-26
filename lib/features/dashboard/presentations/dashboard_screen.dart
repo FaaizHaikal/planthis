b2946bd03540b99b4d39219844629b0661a131d2
FaaizHaikal <faaizhaikal@gmail.com>
2025-06-26 08:26:26 +0700

    feat: new dashboard

--- lib/features/dashboard/presentations/dashboard_screen.dart
+++ lib/features/dashboard/presentations/dashboard_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:planthis/app/theme/app_colors.dart';
+import 'package:planthis/features/dashboard/presentations/widgets/dashboard_button.dart';
 
 class DashboardScreen extends ConsumerWidget {
   const DashboardScreen({super.key});
@@ -32,45 +33,22 @@ class DashboardScreen extends ConsumerWidget {
             // Main Scan Button
             Center(
               child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Ink(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: AppColors.primary,
-                      boxShadow: [
-                        BoxShadow(
-                          color: AppColors.primary.withOpacity(0.4),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                        ),
-                      ],
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/lahan-saya');
-                      },
-                      borderRadius: BorderRadius.circular(100),
-                      child: const SizedBox(
-                        width: 200,
-                        height: 200,
-                        child: Center(
-                          child: FaIcon(
-                            FontAwesomeIcons.seedling,
-                            size: 100,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 12),
-                  const Text(
-                    "Scan Area",
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                DashboardButton(
+                  icon: FontAwesomeIcons.seedling,
+                  label: "Scan Area",
+                  onTap: () => Navigator.pushNamed(context, '/lahan-saya'),
+                ),
+                const SizedBox(height: 24),
+                DashboardButton(
+                  icon: FontAwesomeIcons.tree,
+                  label: "Ilmu Padi",
+                  onTap: () => Navigator.pushNamed(context, '/ilmu-padi'),
+                ),
+              ],
+            ),
+
             ),
 
             const Spacer(),
